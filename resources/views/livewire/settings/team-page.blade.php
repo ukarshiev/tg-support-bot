d1440733da096e2ecce8d293bc0ad7eac2711731
Илья Лящук <40496434+prog-time@users.noreply.github.com>
2026-06-01 20:31:38 +0300

    issues-148 | make Команда invite button fit-content width

--- resources/views/livewire/settings/team-page.blade.php
+++ resources/views/livewire/settings/team-page.blade.php
@@ -74,7 +74,7 @@
                 wire:click="invite"
                 wire:loading.attr="disabled"
                 wire:target="invite"
-                class="h-[42px] w-full shrink-0 sm:w-auto"
+                class="h-[42px] w-fit shrink-0"
             >
                 <svg xmlns="http://www.w3.org/2000/svg" class="mr-1.5 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
